3c346a0af1e7fff5b339636b5b84041e3e1c2a22
Marvin Valenzuela <bend_@outlook.com>
2018-12-04 21:51:06 -0600

    crude implementation of DFS

--- classes/Vertex.js
+++ classes/Vertex.js
@@ -1,32 +1,59 @@
 
 class Vertex {
-    constructor(x, y) {
+    constructor(x, y, index) {
         this.x = x;
         this.y = y;
-        this.radius = 25;
+        this.index = index;
+        this.radius = 40;
 
         this.neighbors = [];
+        this.parent = null;
 
         this.selected = false;
+
+        this.search = [];
     }
 
-    linkToVertex(vertex){
-        this.neighbors.push(vertex);
+    linkToVertex(vertex) {
+        if (!this.neighbors.includes(vertex)) {
+            this.neighbors.push(vertex);
+            this.neighbors.sort(function (a, b) { return a.index - b.index });
+            //console.log(this.neighbors);
+        }
+    }
+
+    linkSearch(vertex) {
+        this.search.push(vertex);
     }
 
     show() {
-        if (this.selected){
-            fill(100);
-        }
-        else{
-            fill(255);
-        }
-        
         stroke(0);
         strokeWeight(3);
-        for (let neighbor of this.neighbors){
+        for (let neighbor of this.neighbors) {
             line(this.x, this.y, neighbor.x, neighbor.y);
         }
+
+        stroke(222, 0, 0);
+        strokeWeight(6);
+        for (let so of this.search) {
+            line(this.x, this.y, so.x, so.y);
+        }
+
+        stroke(0);
+        strokeWeight(3);
+        if (this.selected) {
+            fill(100);
+        }
+        else {
+            fill(255);
+        }
         ellipse(this.x, this.y, this.radius);
+
+        noStroke();
+        fill(0);
+        textAlign(CENTER);
+        textSize(20);
+        textFont('cursive');
+        text(this.index, this.x, this.y + 5);
     }
 }
--- sketch.js
+++ sketch.js
@@ -4,12 +4,17 @@ let vertices;
 let selectedVertex;
 let isVertexSelected;
 
+let displayDepthSearch;
+
 function setup() {
     createCanvas(displayWidth, displayHeight);
     this.mouseCollision = new Rectangle(0, 0, 1, 1);
     vertices = [];
-    selectedVertex = new Vertex(0, 0);
+    selectedVertex = null;
     isVertexSelected = false;
+
+    search = [];
+    displayDepthSearch = false;
 }
 
 function mousePressed() {
@@ -18,28 +23,63 @@ function mousePressed() {
     for (let vertex of vertices) {
         if (mouseCollidesWithVertex(vertex)) {
             collided = true;
-            if (!isVertexSelected){
+            if (!isVertexSelected) {
                 selectedVertex = vertex;
                 selectedVertex.selected = true;
                 isVertexSelected = true;
             }
             else {
-                selectedVertex.linkToVertex(vertex);
-                selectedVertex.selected = false;
-                isVertexSelected = false;
+                if (!(vertex === selectedVertex)) {
+                    selectedVertex.linkToVertex(vertex);
+                    vertex.linkToVertex(selectedVertex);
+                    selectedVertex.selected = false;
+                    isVertexSelected = false;
+                }
             }
             break;
         }
     }
 
     if (!collided) {
-        vertices.push(new Vertex(mouseX, mouseY));
+        vertices.push(new Vertex(mouseX, mouseY, vertices.length + 1));
+        vertices.sort(function (a, b) { return b.y - a.y });
     }
 }
 
 function mouseCollidesWithVertex(vertex) {
     let distance = dist(this.mouseCollision.x, this.mouseCollision.y, vertex.x, vertex.y);
     return distance < vertex.radius;
+}
+
+function keyPressed() {
+    // Space
+    if (keyCode === 32) {
+        vertices = [];
+    }
+    // Enter
+    if (keyCode === 13) {
+        depthFirstSearch();
+        displayDepthSearch = true;
+    }
+}
+
+function depthFirstSearch() {
+    let stack = [];
+    let explored = [];
+    stack.push(vertices[0]);
+    while (stack.length > 0) {
+        let vertex = stack.pop();
+        if (!explored.includes(vertex)) {
+            if (vertex.parent != null){
+                vertex.linkSearch(vertex.parent);
+            }
+            explored.push(vertex);
+            for (let neighbor of vertex.neighbors) {
+                neighbor.parent = vertex;
+                stack.push(neighbor);
+            }
+        }
+    }
 }
 
 function draw() {
