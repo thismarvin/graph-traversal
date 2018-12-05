76329bb556e309f3eafbffb233fac59aca63f5eb
Marvin Valenzuela <bend_@outlook.com>
2018-12-04 19:37:57 -0600

    added ability to select and link vertices

--- sketch.js
+++ sketch.js
@@ -1,20 +1,49 @@
 
-const canvasWidth = 1280;
-const canvasHeight = 720;
-
+let mouseCollision;
 let vertices;
+let selectedVertex;
+let isVertexSelected;
 
 function setup() {
-    createCanvas(canvasWidth, canvasHeight);
+    createCanvas(displayWidth, displayHeight);
+    this.mouseCollision = new Rectangle(0, 0, 1, 1);
     vertices = [];
+    selectedVertex = new Vertex(0, 0);
+    isVertexSelected = false;
 }
 
 function mousePressed() {
-    vertices.push(new Vertex(mouseX, mouseY));
+    this.mouseCollision.setLocation(mouseX, mouseY);
+    let collided = false;
+    for (let vertex of vertices) {
+        if (mouseCollidesWithVertex(vertex)) {
+            collided = true;
+            if (!isVertexSelected){
+                selectedVertex = vertex;
+                selectedVertex.selected = true;
+                isVertexSelected = true;
+            }
+            else {
+                selectedVertex.linkToVertex(vertex);
+                selectedVertex.selected = false;
+                isVertexSelected = false;
+            }
+            break;
+        }
+    }
+
+    if (!collided) {
+        vertices.push(new Vertex(mouseX, mouseY));
+    }
+}
+
+function mouseCollidesWithVertex(vertex) {
+    let distance = dist(this.mouseCollision.x, this.mouseCollision.y, vertex.x, vertex.y);
+    return distance < vertex.radius;
 }
 
 function draw() {
-    background(0);
+    background(255);
     for (let vertex of vertices) {
         vertex.show();
     }
