4e45cd3f10f857432201ca4f8c2c5ddb14ae7b89
Marvin Valenzuela <bend_@outlook.com>
2018-12-04 19:01:23 -0600

    ability to add and show vertices

--- sketch.js
+++ sketch.js
@@ -1,11 +1,22 @@
 
+const canvasWidth = 1280;
+const canvasHeight = 720;
 
+let vertices;
 
 function setup() {
+    createCanvas(canvasWidth, canvasHeight);
+    vertices = [];
+}
 
+function mousePressed() {
+    vertices.push(new Vertex(mouseX, mouseY));
 }
 
 function draw() {
-
+    background(0);
+    for (let vertex of vertices) {
+        vertex.show();
+    }
 }
 
