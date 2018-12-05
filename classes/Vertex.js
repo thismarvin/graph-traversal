ff6ef1aba8ed66fce0fd1b8c88ac968d00292876
Marvin Valenzuela <bend_@outlook.com>
2018-12-04 19:37:34 -0600

    added ability to add linked vertices

--- classes/Vertex.js
+++ classes/Vertex.js
@@ -4,11 +4,29 @@ class Vertex {
         this.x = x;
         this.y = y;
         this.radius = 25;
+
+        this.neighbors = [];
+
+        this.selected = false;
+    }
+
+    linkToVertex(vertex){
+        this.neighbors.push(vertex);
     }
 
     show() {
-        fill(255);
+        if (this.selected){
+            fill(100);
+        }
+        else{
+            fill(255);
+        }
+        
+        stroke(0);
+        strokeWeight(3);
+        for (let neighbor of this.neighbors){
+            line(this.x, this.y, neighbor.x, neighbor.y);
+        }
         ellipse(this.x, this.y, this.radius);
     }
-
 }
